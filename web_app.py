c690cb2884d92ed15dca9e1fd0d18522d75d2a48
Your Name <you@example.com>
2024-04-06 11:49:41 +0300

    Created index site

--- web_app.py
+++ web_app.py
@@ -1,3 +1,15 @@
-#init commit
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
-print("Hello my web_app!!!")
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
